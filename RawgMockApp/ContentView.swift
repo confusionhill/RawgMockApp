1da2746bdd62a6eb875d3f7d28fdbd4665a60f49
Fdika24 <71744284+Fdika24@users.noreply.github.com>
2021-08-04 22:49:21 +0700

    Initial Commit

---
+++ RawgMockApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RawgMockApp
+//
+//  Created by Farhandika Zahrir Mufti guenia on 04/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
